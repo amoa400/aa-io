6bf75c2822eb0c951fda8599a7440d03659cc9c0
amoa400 <amoa400@gmail.com>
2014-04-20 21:48:16 +0800

    init

---
+++ test.js
@@ -0,0 +1,30 @@
+var aaio = require('./index');
+var log = require('util').log;
+
+log('aa-io listening on port 6678');
+var server = aaio.listen(6678, {
+  authorizeFunc: function(token) {
+    return token == '123';
+  }
+});
+
+server.on('connection', function(socket) {
+  log('new connection, id: ' + socket.id);
+});
+
+server.on('message', function(socket, message) {
+  log('message, id: ' + socket.id + ', content: ' + JSON.stringify(message));
+  socket.send({received: 1, data: message.n});
+});
+
+server.on('leave', function(socket) {
+  log('leave, id: ' + socket.id);
+});
+
+setInterval(function() {
+  var len = 0;
+  for (var i in server.clients) {
+    len++;
+  }
+  log('total clients: ' + len);
+}, 5000);
